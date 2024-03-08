1587720e83538dcfa374f59487d4216627fe2fe8
Vidarsha Wagaarachchi <127051176+Vidarsha98@users.noreply.github.com>
2024-03-08 17:19:34 +0530

    Add files via upload

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace hospital_management
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            panelsidebar = new Panel();
+            pictureBoxdashboard = new PictureBox();
+            lbldashboard = new Label();
+            lbllogout = new Label();
+            pictureBox9 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            lblsidebarreceptionist = new Label();
+            lblsidebarlaboratory = new Label();
+            lblsidebardoctor = new Label();
+            lblsidebarpatient = new Label();
+            pictureBoxlogo = new PictureBox();
+            panelbottom = new Panel();
+            panelsidebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxdashboard).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxlogo).BeginInit();
+            SuspendLayout();
+            // 
+            // panelsidebar
+            // 
+            panelsidebar.BackColor = Color.DarkSlateGray;
+            panelsidebar.BackgroundImageLayout = ImageLayout.None;
+            panelsidebar.Controls.Add(pictureBoxdashboard);
+            panelsidebar.Controls.Add(lbldashboard);
+            panelsidebar.Controls.Add(lbllogout);
+            panelsidebar.Controls.Add(pictureBox9);
+            panelsidebar.Controls.Add(pictureBox8);
+            panelsidebar.Controls.Add(pictureBox7);
+            panelsidebar.Controls.Add(pictureBox6);
+            panelsidebar.Controls.Add(pictureBox2);
+            panelsidebar.Controls.Add(lblsidebarreceptionist);
+            panelsidebar.Controls.Add(lblsidebarlaboratory);
+            panelsidebar.Controls.Add(lblsidebardoctor);
+            panelsidebar.Controls.Add(lblsidebarpatient);
+            panelsidebar.Controls.Add(pictureBoxlogo);
+            panelsidebar.Dock = DockStyle.Left;
+            panelsidebar.Location = new Point(0, 0);
+            panelsidebar.Name = "panelsidebar";
+            panelsidebar.Size = new Size(200, 724);
+            panelsidebar.TabIndex = 1;
+            // 
+            // pictureBoxdashboard
+            // 
+            pictureBoxdashboard.BackColor = Color.White;
+            pictureBoxdashboard.Image = (Image)resources.GetObject("pictureBoxdashboard.Image");
+            pictureBoxdashboard.Location = new Point(13, 159);
+            pictureBoxdashboard.Name = "pictureBoxdashboard";
+            pictureBoxdashboard.Size = new Size(53, 57);
+            pictureBoxdashboard.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxdashboard.TabIndex = 17;
+            pictureBoxdashboard.TabStop = false;
+            // 
+            // lbldashboard
+            // 
+            lbldashboard.AutoSize = true;
+            lbldashboard.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbldashboard.ForeColor = Color.White;
+            lbldashboard.Location = new Point(91, 180);
+            lbldashboard.Name = "lbldashboard";
+            lbldashboard.Size = new Size(92, 21);
+            lbldashboard.TabIndex = 16;
+            lbldashboard.Text = "Dashboard";
+            // 
+            // lbllogout
+            // 
+            lbllogout.AutoSize = true;
+            lbllogout.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbllogout.ForeColor = Color.White;
+            lbllogout.Location = new Point(57, 649);
+            lbllogout.Name = "lbllogout";
+            lbllogout.Size = new Size(55, 17);
+            lbllogout.TabIndex = 15;
+            lbllogout.Text = "Logout";
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.Transparent;
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(59, 611);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(53, 35);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 14;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.Honeydew;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(13, 464);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(50, 54);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 13;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.Honeydew;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(12, 312);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(53, 53);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 12;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Honeydew;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(13, 388);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(50, 53);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Honeydew;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(13, 238);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(53, 57);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // lblsidebarreceptionist
+            // 
+            lblsidebarreceptionist.AutoSize = true;
+            lblsidebarreceptionist.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblsidebarreceptionist.ForeColor = Color.White;
+            lblsidebarreceptionist.Location = new Point(81, 497);
+            lblsidebarreceptionist.Name = "lblsidebarreceptionist";
+            lblsidebarreceptionist.Size = new Size(103, 21);
+            lblsidebarreceptionist.TabIndex = 10;
+            lblsidebarreceptionist.Text = "Receptionist";
+            // 
+            // lblsidebarlaboratory
+            // 
+            lblsidebarlaboratory.AutoSize = true;
+            lblsidebarlaboratory.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblsidebarlaboratory.ForeColor = Color.White;
+            lblsidebarlaboratory.Location = new Point(91, 420);
+            lblsidebarlaboratory.Name = "lblsidebarlaboratory";
+            lblsidebarlaboratory.Size = new Size(93, 21);
+            lblsidebarlaboratory.TabIndex = 8;
+            lblsidebarlaboratory.Text = "Laboratory";
+            // 
+            // lblsidebardoctor
+            // 
+            lblsidebardoctor.AutoSize = true;
+            lblsidebardoctor.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblsidebardoctor.ForeColor = Color.White;
+            lblsidebardoctor.Location = new Point(96, 344);
+            lblsidebardoctor.Name = "lblsidebardoctor";
+            lblsidebardoctor.Size = new Size(63, 21);
+            lblsidebardoctor.TabIndex = 7;
+            lblsidebardoctor.Text = "Doctor";
+            // 
+            // lblsidebarpatient
+            // 
+            lblsidebarpatient.AutoSize = true;
+            lblsidebarpatient.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblsidebarpatient.ForeColor = Color.White;
+            lblsidebarpatient.Location = new Point(91, 274);
+            lblsidebarpatient.Name = "lblsidebarpatient";
+            lblsidebarpatient.Size = new Size(68, 21);
+            lblsidebarpatient.TabIndex = 6;
+            lblsidebarpatient.Text = "Patients";
+            // 
+            // pictureBoxlogo
+            // 
+            pictureBoxlogo.BackColor = Color.Transparent;
+            pictureBoxlogo.BackgroundImage = (Image)resources.GetObject("pictureBoxlogo.BackgroundImage");
+            pictureBoxlogo.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBoxlogo.Location = new Point(0, 0);
+            pictureBoxlogo.Name = "pictureBoxlogo";
+            pictureBoxlogo.Size = new Size(200, 124);
+            pictureBoxlogo.TabIndex = 2;
+            pictureBoxlogo.TabStop = false;
+            // 
+            // panelbottom
+            // 
+            panelbottom.BackColor = Color.DarkSlateGray;
+            panelbottom.Location = new Point(200, 695);
+            panelbottom.Name = "panelbottom";
+            panelbottom.Size = new Size(1114, 29);
+            panelbottom.TabIndex = 77;
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1314, 724);
+            Controls.Add(panelbottom);
+            Controls.Add(panelsidebar);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Dashboard";
+            Text = "Dashboard";
+            panelsidebar.ResumeLayout(false);
+            panelsidebar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxdashboard).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxlogo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelsidebar;
+        private PictureBox pictureBoxdashboard;
+        private Label lbldashboard;
+        private Label lbllogout;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox2;
+        private Label lblsidebarreceptionist;
+        private Label lblsidebarlaboratory;
+        private Label lblsidebardoctor;
+        private Label lblsidebarpatient;
+        private PictureBox pictureBoxlogo;
+        private Panel panelbottom;
+    }
+}
