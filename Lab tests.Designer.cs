7a4828a92a73936f6428ffbd2dbf483fb9684e9b
Vidarsha Wagaarachchi <127051176+Vidarsha98@users.noreply.github.com>
2024-03-08 17:21:42 +0530

    Add files via upload

---
+++ Lab tests.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace hospital_management
+{
+    partial class Lab_tests
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lab_tests));
+            panelsidebar = new Panel();
+            lbllogout = new Label();
+            pictureBox9 = new PictureBox();
+            pictureBoxlogo = new PictureBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            lblsidebarlaboratory = new Label();
+            pictureBoxclose = new PictureBox();
+            lbltestname = new Label();
+            txttestname = new TextBox();
+            txtcost = new TextBox();
+            lblcost = new Label();
+            dataGridViewlabtest = new DataGridView();
+            panelbottom = new Panel();
+            btnedit = new Button();
+            btndelete = new Button();
+            btnadd = new Button();
+            panelsidebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxlogo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxclose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewlabtest).BeginInit();
+            SuspendLayout();
+            // 
+            // panelsidebar
+            // 
+            panelsidebar.BackColor = Color.DarkSlateGray;
+            panelsidebar.BackgroundImageLayout = ImageLayout.None;
+            panelsidebar.Controls.Add(label1);
+            panelsidebar.Controls.Add(pictureBox1);
+            panelsidebar.Controls.Add(lbllogout);
+            panelsidebar.Controls.Add(pictureBox9);
+            panelsidebar.Controls.Add(pictureBoxlogo);
+            panelsidebar.Dock = DockStyle.Left;
+            panelsidebar.Location = new Point(0, 0);
+            panelsidebar.Name = "panelsidebar";
+            panelsidebar.Size = new Size(142, 534);
+            panelsidebar.TabIndex = 26;
+            // 
+            // lbllogout
+            // 
+            lbllogout.AutoSize = true;
+            lbllogout.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbllogout.ForeColor = Color.White;
+            lbllogout.Location = new Point(62, 750);
+            lbllogout.Name = "lbllogout";
+            lbllogout.Size = new Size(64, 21);
+            lbllogout.TabIndex = 15;
+            lbllogout.Text = "Logout";
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.Transparent;
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(62, 710);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(54, 36);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 14;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBoxlogo
+            // 
+            pictureBoxlogo.BackColor = Color.Transparent;
+            pictureBoxlogo.BackgroundImage = (Image)resources.GetObject("pictureBoxlogo.BackgroundImage");
+            pictureBoxlogo.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBoxlogo.Location = new Point(-10, 0);
+            pictureBoxlogo.Name = "pictureBoxlogo";
+            pictureBoxlogo.Size = new Size(152, 115);
+            pictureBoxlogo.TabIndex = 2;
+            pictureBoxlogo.TabStop = false;
+            pictureBoxlogo.Click += pictureBoxlogo_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(39, 498);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 17);
+            label1.TabIndex = 17;
+            label1.Text = "Logout";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(39, 466);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(40, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Honeydew;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(387, 47);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(44, 42);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 28;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // lblsidebarlaboratory
+            // 
+            lblsidebarlaboratory.AutoSize = true;
+            lblsidebarlaboratory.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblsidebarlaboratory.ForeColor = Color.Black;
+            lblsidebarlaboratory.Location = new Point(345, 22);
+            lblsidebarlaboratory.Name = "lblsidebarlaboratory";
+            lblsidebarlaboratory.Size = new Size(143, 22);
+            lblsidebarlaboratory.TabIndex = 27;
+            lblsidebarlaboratory.Text = "LABORATORY";
+            lblsidebarlaboratory.Click += lblsidebarlaboratory_Click;
+            // 
+            // pictureBoxclose
+            // 
+            pictureBoxclose.BackColor = Color.Transparent;
+            pictureBoxclose.BackgroundImage = (Image)resources.GetObject("pictureBoxclose.BackgroundImage");
+            pictureBoxclose.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBoxclose.Location = new Point(676, 5);
+            pictureBoxclose.Name = "pictureBoxclose";
+            pictureBoxclose.Size = new Size(40, 21);
+            pictureBoxclose.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxclose.TabIndex = 29;
+            pictureBoxclose.TabStop = false;
+            // 
+            // lbltestname
+            // 
+            lbltestname.AutoSize = true;
+            lbltestname.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbltestname.Location = new Point(184, 155);
+            lbltestname.Name = "lbltestname";
+            lbltestname.Size = new Size(79, 17);
+            lbltestname.TabIndex = 30;
+            lbltestname.Text = "Test Name";
+            // 
+            // txttestname
+            // 
+            txttestname.Location = new Point(184, 175);
+            txttestname.Name = "txttestname";
+            txttestname.Size = new Size(178, 23);
+            txttestname.TabIndex = 31;
+            // 
+            // txtcost
+            // 
+            txtcost.Location = new Point(440, 175);
+            txtcost.Name = "txtcost";
+            txtcost.Size = new Size(155, 23);
+            txtcost.TabIndex = 33;
+            // 
+            // lblcost
+            // 
+            lblcost.AutoSize = true;
+            lblcost.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblcost.Location = new Point(440, 155);
+            lblcost.Name = "lblcost";
+            lblcost.Size = new Size(39, 17);
+            lblcost.TabIndex = 34;
+            lblcost.Text = "Cost";
+            // 
+            // dataGridViewlabtest
+            // 
+            dataGridViewlabtest.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewlabtest.Location = new Point(148, 247);
+            dataGridViewlabtest.Name = "dataGridViewlabtest";
+            dataGridViewlabtest.RowTemplate.Height = 25;
+            dataGridViewlabtest.Size = new Size(557, 259);
+            dataGridViewlabtest.TabIndex = 35;
+            // 
+            // panelbottom
+            // 
+            panelbottom.BackColor = Color.DarkSlateGray;
+            panelbottom.Dock = DockStyle.Bottom;
+            panelbottom.Location = new Point(142, 512);
+            panelbottom.Name = "panelbottom";
+            panelbottom.Size = new Size(575, 22);
+            panelbottom.TabIndex = 36;
+            // 
+            // btnedit
+            // 
+            btnedit.BackColor = Color.LimeGreen;
+            btnedit.BackgroundImageLayout = ImageLayout.None;
+            btnedit.FlatStyle = FlatStyle.Flat;
+            btnedit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnedit.ForeColor = SystemColors.ButtonHighlight;
+            btnedit.Location = new Point(440, 218);
+            btnedit.Name = "btnedit";
+            btnedit.Size = new Size(75, 23);
+            btnedit.TabIndex = 55;
+            btnedit.Text = "Edit";
+            btnedit.UseVisualStyleBackColor = false;
+            // 
+            // btndelete
+            // 
+            btndelete.BackColor = Color.Red;
+            btndelete.FlatStyle = FlatStyle.Flat;
+            btndelete.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btndelete.ForeColor = SystemColors.ButtonHighlight;
+            btndelete.Location = new Point(356, 218);
+            btndelete.Name = "btndelete";
+            btndelete.Size = new Size(75, 23);
+            btndelete.TabIndex = 54;
+            btndelete.Text = "Delete";
+            btndelete.UseVisualStyleBackColor = false;
+            // 
+            // btnadd
+            // 
+            btnadd.BackColor = Color.Black;
+            btnadd.BackgroundImageLayout = ImageLayout.None;
+            btnadd.FlatStyle = FlatStyle.Flat;
+            btnadd.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnadd.ForeColor = SystemColors.ButtonHighlight;
+            btnadd.Location = new Point(268, 218);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(75, 23);
+            btnadd.TabIndex = 53;
+            btnadd.Text = "Add";
+            btnadd.UseVisualStyleBackColor = false;
+            // 
+            // Lab_tests
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(717, 534);
+            Controls.Add(btnedit);
+            Controls.Add(btndelete);
+            Controls.Add(btnadd);
+            Controls.Add(panelbottom);
+            Controls.Add(dataGridViewlabtest);
+            Controls.Add(lblcost);
+            Controls.Add(txtcost);
+            Controls.Add(txttestname);
+            Controls.Add(lbltestname);
+            Controls.Add(pictureBoxclose);
+            Controls.Add(pictureBox6);
+            Controls.Add(lblsidebarlaboratory);
+            Controls.Add(panelsidebar);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Lab_tests";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Lab_tests";
+            panelsidebar.ResumeLayout(false);
+            panelsidebar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxlogo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxclose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewlabtest).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panelsidebar;
+        private Label lbllogout;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBoxlogo;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox6;
+        private Label lblsidebarlaboratory;
+        private PictureBox pictureBoxclose;
+        private Label lbltestname;
+        private TextBox txttestname;
+        private TextBox txtcost;
+        private Label lblcost;
+        private DataGridView dataGridViewlabtest;
+        private Panel panelbottom;
+        private Button btnedit;
+        private Button btndelete;
+        private Button btnadd;
+    }
+}
---
+++ Lab tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hospital_management
+{
+    public partial class Lab_tests : Form
+    {
+        public Lab_tests()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxlogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblsidebarlaboratory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
